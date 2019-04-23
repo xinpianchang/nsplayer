cba57922c8ee8cdcf5c691a793554afd8e4e3bf2
xg4 <xingor4@gmail.com>
2019-04-23 14:39:35 +0800

    chore(deps): add rollup, babel and typescript feat: add sum func

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    },
+    {
+      name: 'demo',
+      file: pkg.browser,
+      format: 'umd'
+    }
+  ],
+  plugins: [
+    resolve({ extensions }),
+    commonjs(),
+    babel({
+      extensions,
+      include: ['src/**/*']
+    }),
+    terser()
+  ]
+}
