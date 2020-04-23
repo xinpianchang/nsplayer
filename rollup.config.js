84462391da612bf9d7f73f82ca52e4e1f6318cce
xg4 <xingor4@gmail.com>
2020-04-23 23:28:26 +0800

    chore(deps): update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
@@ -11,25 +11,25 @@ export default {
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: 'cjs',
     },
     {
       file: pkg.module,
-      format: 'esm'
+      format: 'esm',
     },
     {
-      name: pkg.name,
+      name: 'boilerplate',
       file: pkg.browser,
-      format: 'umd'
-    }
+      format: 'umd',
+    },
   ],
   plugins: [
     resolve({ extensions }),
     commonjs(),
     babel({
       extensions,
-      include: ['src/**/*']
+      include: ['src/**/*'],
     }),
-    terser()
-  ]
+    terser(),
+  ],
 }
